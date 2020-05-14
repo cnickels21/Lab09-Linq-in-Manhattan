81629f3fc6f0e904c792bd453116b21c01e2ff64
cnickels21 <cnickels_21@yahoo.com>
2020-05-14 16:23:51 -0500

    geometry class for objectifying json

--- Lab09LinqInManhattan.Tests/Lab09LinqInManhattanTests.cs
+++ Lab09LinqInManhattan.Tests/Lab09LinqInManhattanTests.cs
@@ -1,9 +1,8 @@
-using System;
 using Xunit;
 
 namespace Lab09LinqInManhattan.Tests
 {
-    public class UnitTest1
+    public class Lab09LinqInManhattanTests
     {
         [Fact]
         public void Test1()
